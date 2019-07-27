d3280dc96078a4056384e54a5fff04d6a44b372f
Hans-Jörg Wieland <mail@wieland.tech>
2019-07-27 16:46:52 +0200

    Remove key pair, worker disk size (causing errors) and add commented data resource for nat gateway

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "alicloud_zones" main {
 }
 
 resource "alicloud_key_pair" "publickey" {
-  key_name   = var.ssh_key_pair_name
+  # key_name   = var.ssh_key_pair_name
   public_key = file(var.ssh_public_key_path)
 }
 
@@ -36,7 +36,7 @@ resource "alicloud_vpc" "vpc" {
 resource "alicloud_vswitch" "vswitches" {
   count             = var.enable_alibaba ? length(var.ali_vswitch_cidrs) : 0
   vpc_id            = alicloud_vpc.vpc[count.index].id
-  cidr_block        = element(var.ali_vswitch_cidrs, count.index)
+  cidr_block        = var.ali_vswitch_cidrs[count.index]
   availability_zone = lookup(data.alicloud_zones.main[count.index].zones[count.index % length(data.alicloud_zones.main[count.index].zones)], "id")
   name              = "${var.ack_name}-${random_id.cluster_name.hex}"
 
@@ -53,6 +53,15 @@ resource "alicloud_nat_gateway" "default" {
   depends_on = [alicloud_vswitch.vswitches]
 }
 
+## Data source for getting a NAT gateway
+//data "alicloud_nat_gateways" "default" {
+//  count = var.enable_alibaba ? 1 : 0
+//  vpc_id     = alicloud_vpc.vpc[count.index].id
+//  name_regex = "k8s-ali-.*"
+//  #ids        = ["${alicloud_nat_gateway.foo.id}"]
+//}
+
+
 
 resource "alicloud_eip" "eip" {
   count     = var.enable_alibaba ? 1 : 0
@@ -64,13 +73,15 @@ resource "alicloud_eip_association" "eipassoc" {
   count         = var.enable_alibaba ? 1 : 0
   allocation_id = alicloud_eip.eip[count.index].id
   instance_id   = alicloud_nat_gateway.default[count.index].id
+  #instance_id = data.alicloud_nat_gateways.default[count.index].id
 }
 
 
 resource "alicloud_snat_entry" "default" {
   count             = var.enable_alibaba ? length(var.ali_vswitch_cidrs) : 0
   snat_table_id     = alicloud_nat_gateway.default[count.index].snat_table_ids
-  source_vswitch_id = element(split(",", join(",", alicloud_vswitch.vswitches.*.id)), count.index % length(split(",", join(",", alicloud_vswitch.vswitches.*.id))))
+  #snat_table_id = data.alicloud_nat_gateways.default[count.index].gateways.0.snat_table_id
+  source_vswitch_id = split(",", join(",", alicloud_vswitch.vswitches.*.id))[count.index % length(split(",", join(",", alicloud_vswitch.vswitches.*.id)))]
   snat_ip           = alicloud_eip.eip[count.index].ip_address
 }
 
@@ -80,21 +91,16 @@ resource "alicloud_cs_managed_kubernetes" "ack" {
 
   name                      = "${var.ack_name}-${random_id.cluster_name.hex}"
   availability_zone         = data.alicloud_zones.main[count.index].zones.0.id
-  vswitch_ids               = [element(split(",", join(",", alicloud_vswitch.vswitches.*.id)), count.index % length(split(",", join(",", alicloud_vswitch.vswitches.*.id))))]
+  vswitch_ids               = [split(",", join(",", alicloud_vswitch.vswitches.*.id))[count.index % length(split(",", join(",", alicloud_vswitch.vswitches.*.id)))]]
   new_nat_gateway           = false
   worker_instance_types     = [var.ack_node_type]
-  worker_numbers            = var.ack_node_count
-  key_name                  = var.ssh_key_pair_name
+  worker_numbers            = [var.ack_node_count]
+  key_name                  = alicloud_key_pair.publickey.key_name
   pod_cidr                  = var.ack_k8s_pod_cidr
   service_cidr              = var.ack_k8s_service_cidr
   install_cloud_monitor     = true
   slb_internet_enabled      = true
-  worker_disk_category      = var.ack_worker_system_disk_category
-  worker_disk_size          = var.ack_worker_system_disk_size
-  worker_data_disk_size     = var.ack_worker_data_disk_size
-  worker_data_disk_category = var.ack_worker_data_disk_category
   cluster_network_type      = var.ack_k8s_cni
-
   kube_config = "./kubeconfig_ack"
 
   depends_on = ["alicloud_snat_entry.default"]
