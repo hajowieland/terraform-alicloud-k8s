a2062eb63446b0d2fd570dcc8d1dcbbe9a4bf87a
hajowieland <mail@wieland.tech>
2019-08-11 02:21:35 +0200

    fix kubeconfig path suffix

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "alicloud_eip_ip_address" {
   value = alicloud_eip.eip.0.ip_address
 }
 
-output "kubeconfig_path_oci" {
+output "kubeconfig_path_ali" {
   value = alicloud_cs_managed_kubernetes.ack.*.kube_config
 }
