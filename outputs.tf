8ce9da1db3f11f47ef547612ba90cbc865fedb32
hajowieland <mail@wieland.tech>
2019-08-11 15:16:17 +0200

    Use first value of count for outputs (multi-cloud module)

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "alicloud_eip_ip_address" {
 }
 
 output "kubeconfig_path_ali" {
-  value = alicloud_cs_managed_kubernetes.ack.*.kube_config
+  value = alicloud_cs_managed_kubernetes.ack.0.kube_config
 }
